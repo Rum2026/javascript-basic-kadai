79d6b885b6ba2fdd43811e2ee397d3332b01648a
Rum2026 <maho_massimo@hotmail.com>
2026-05-03 22:16:21 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,10 @@
+let bodyWeight;
+bodyWeight = 68;
+
+let bodyHeight;
+bodyHeight = 1.7;
+
+let bodyMassIndex;
+bodyMassIndex = bodyWeight / (bodyHeight * bodyHeight);
+
+console.log(bodyMassIndex);
